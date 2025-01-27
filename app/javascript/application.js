7f6e83303519bb67e55efe086c5ad92602633c14
Kyle Mallette <kemallette@gmail.com>
2025-01-26 22:05:44 -0800

    Finished forge and about page. Init'd demo and careers.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+$(document).ready(function() {
+  // Optional: Show the first panel by default
+  $('.tab').first().addClass('bg-white border-l-[1px] border-t-[1px] border-b-[1px]');
+  $('.tab-panel').hide().first().show();
+
+  $('.tab').on('click', function() {
+    // Remove highlight from all tabs
+    $('.tab').removeClass('bg-white border-l-[1px] border-t-[1px] border-b-[1px]');
+
+    // Highlight this tab
+    $(this).addClass('bg-white border-l-[1px] border-t-[1px] border-b-[1px]');
+
+    // Hide all panels
+    $('.tab-panel').addClass('hidden').hide();
+
+    // Show the targeted panel
+    const targetPanel = $(this).data('panel');
+    $('#' + targetPanel).removeClass('hidden').show();
+  });
+});
